14d452121378ae3dec057759f70823fde18c40b5
Omar Alvarez <omajul2@hotmail.com>
2015-10-01 15:58:52 +0200

    Introduction chapter done

---
+++ Introduction/intro.rb
@@ -0,0 +1,41 @@
+# Set the following variables
+
+my_num = 25 
+
+my_boolean = true
+
+my_string = "Ruby"
+
+a = 10
+b = 6
+c = a % b
+
+=begin 
+THIS IS A MULTI LINE COMMENT
+'puts' and 'print'
+print command just takes whatever you give and prints it to the screen
+puts adds a new blank line after the print
+=end
+
+print my_string
+print " rocks "
+puts "Don't you think ?"
+
+# Everything on ruby is an object !
+
+puts "This string is already an object that has their own methods".length
+puts "OmaR".reverse
+puts "lower".upcase
+puts "UPPER".downcase
+
+name = "omar"
+puts name.capitalize
+
+name = "laUrA"
+puts name.capitalize.downcase.reverse.upcase
+
+# MATHS
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
